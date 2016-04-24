06c515de7da8a85df0c952f5631b8c89f706b0ce
Nishant Bhasin <nishantbhasin@cmail.carleton.ca>
2016-04-24 19:36:49 -0400

    Initial Commit

---
+++ Snackbar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snackbar
+//
+//  Created by Nishant on 2016-04-24.
+//  Copyright © 2016 Epicara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
